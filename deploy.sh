5de4cb5908997bdc52f429863c81c9c6d212c494
rezkyrafael2901 <redoaldi34@gmail.com>
2026-08-11 03:31:51 +0800

    Remove token from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export VERCEL_TOKEN="your...
+# Set VERCEL_TOKEN in your environment before running this script
+# export VERCEL_TOKEN="your...
 cd /home/ubuntu/nodea-v2
 npx vercel deploy --prod --yes 2>&1
